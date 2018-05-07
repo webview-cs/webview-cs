4c33107b7c238e1da9ea24d691f75367c4dae5dc
Will Speak <lithiumflame@gmail.com>
2018-05-07 10:15:45 +0100

    Make FFI Methods Internal
    Don't want people palying with these outside this package.

--- Ffi.cs
+++ Ffi.cs
@@ -4,16 +4,16 @@ using System.Runtime.InteropServices;
 namespace webview_cs
 {
     [UnmanagedFunctionPointer(CallingConvention.Cdecl)]
-    public delegate void webview_external_invoke_cb_t(
+    internal delegate void webview_external_invoke_cb_t(
         IntPtr webview,
         [MarshalAs(UnmanagedType.LPUTF8Str)] string arg);
 
-    public static class Ffi
+    internal static class Ffi
     {
         // WEBVIEW_API int webview(const char *title, const char *url, int width,
         //                 int height, int resizable);
         [DllImport("webview", CallingConvention = CallingConvention.Cdecl)]
-        public static extern int webview(
+        internal static extern int webview(
             [MarshalAs(UnmanagedType.LPUTF8Str)] string title,
             [MarshalAs(UnmanagedType.LPUTF8Str)] string url,
             int width,
@@ -25,7 +25,7 @@ namespace webview_cs
         //                                           int resizeable, int debug,
         //                                           webview_external_invoke_cb_t cb);
         [DllImport("webview", CallingConvention = CallingConvention.Cdecl)]
-        public static extern UIntPtr webview_alloc(
+        internal static extern UIntPtr webview_alloc(
             [MarshalAs(UnmanagedType.LPUTF8Str)] string title,
             [MarshalAs(UnmanagedType.LPUTF8Str)] string url,
             int width,
@@ -35,15 +35,15 @@ namespace webview_cs
             webview_external_invoke_cb_t cb);
         // WEBVIEW_API void webview_release(struct webview* webview);
         [DllImport("webview", CallingConvention = CallingConvention.Cdecl)]
-        public static extern void webview_release(UIntPtr webview);
+        internal static extern void webview_release(UIntPtr webview);
 
         // WEBVIEW_API int webview_init(struct webview *w);
         [DllImport("webview", CallingConvention = CallingConvention.Cdecl)]
-        public static extern int webview_init(UIntPtr webview);
+        internal static extern int webview_init(UIntPtr webview);
         
         // WEBVIEW_API int webview_loop(struct webview *w, int blocking);
         [DllImport("webview", CallingConvention = CallingConvention.Cdecl)]
-        public static extern int webview_loop(UIntPtr webview, int blocking);
+        internal static extern int webview_loop(UIntPtr webview, int blocking);
 
         // WEBVIEW_API int webview_eval(struct webview *w, const char *js);
         // WEBVIEW_API int webview_inject_css(struct webview *w, const char *css);
@@ -60,7 +60,7 @@ namespace webview_cs
         // WEBVIEW_API void webview_terminate(struct webview *w);
         // WEBVIEW_API void webview_exit(struct webview *w);
         [DllImport("webview", CallingConvention = CallingConvention.Cdecl)]
-        public static extern void webview_exit(UIntPtr webview);
+        internal static extern void webview_exit(UIntPtr webview);
         // WEBVIEW_API void webview_debug(const char *format, ...);
         // WEBVIEW_API void webview_print_log(const char *s);
     }
