c45c158281c5fc67a8823b2cde6169e72aac1cc0
Will Speak <lithiumflame@gmail.com>
2018-05-07 10:16:00 +0100

    Meddling with Data Uris

--- Program.cs
+++ Program.cs
@@ -3,15 +3,18 @@ using System.Runtime.InteropServices;
 
 namespace webview_cs
 {
+    using System.Text;
     using static Ffi;
 
     class Program
     {
         static void Main(string[] args)
         {
+            var html = @"<html><head><meta charset=""UTF-8""></head><body><h1>hello ü world</h1><script>window.external.invoke(""hello ümlaß world"")</script>";
+            var uri = "data:text/html;base64," + Convert.ToBase64String(Encoding.UTF8.GetBytes(html));
             var webview = webview_alloc(
                 "Foo bar",
-                "https://google.com",
+                uri,
                 1024,
                 768,
                 1,
