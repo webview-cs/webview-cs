ad776b78ba25fd55e20ddcb4652bf788037e67a0
Will Speak <lithiumflame@gmail.com>
2018-05-07 08:51:35 +0100

    Switch to `webview_alloc` API
    This provides an opaque pointer API rather than the simplified API we
    have used so far. This means that the invoke callback is now exposed.

--- Ffi.cs
+++ Ffi.cs
@@ -1,7 +1,13 @@
+using System;
 using System.Runtime.InteropServices;
 
 namespace webview_cs
 {
+    [UnmanagedFunctionPointer(CallingConvention.Cdecl)]
+    public delegate void webview_external_invoke_cb_t(
+        IntPtr webview,
+        [MarshalAs(UnmanagedType.LPUTF8Str)] string arg);
+
     public static class Ffi
     {
         // WEBVIEW_API int webview(const char *title, const char *url, int width,
@@ -13,5 +19,49 @@ namespace webview_cs
             int width,
             int height,
             int resizable);
+        
+        // WEBVIEW_API struct webview* webview_alloc(const char* title, const char* url,
+        //                                           int width, int height,
+        //                                           int resizeable, int debug,
+        //                                           webview_external_invoke_cb_t cb);
+        [DllImport("webview", CallingConvention = CallingConvention.Cdecl)]
+        public static extern UIntPtr webview_alloc(
+            [MarshalAs(UnmanagedType.LPUTF8Str)] string title,
+            [MarshalAs(UnmanagedType.LPUTF8Str)] string url,
+            int width,
+            int height,
+            int resizable,
+            int debug,
+            webview_external_invoke_cb_t cb);
+        // WEBVIEW_API void webview_release(struct webview* webview);
+        [DllImport("webview", CallingConvention = CallingConvention.Cdecl)]
+        public static extern void webview_release(UIntPtr webview);
+
+        // WEBVIEW_API int webview_init(struct webview *w);
+        [DllImport("webview", CallingConvention = CallingConvention.Cdecl)]
+        public static extern int webview_init(UIntPtr webview);
+        
+        // WEBVIEW_API int webview_loop(struct webview *w, int blocking);
+        [DllImport("webview", CallingConvention = CallingConvention.Cdecl)]
+        public static extern int webview_loop(UIntPtr webview, int blocking);
+
+        // WEBVIEW_API int webview_eval(struct webview *w, const char *js);
+        // WEBVIEW_API int webview_inject_css(struct webview *w, const char *css);
+        // WEBVIEW_API void webview_set_title(struct webview *w, const char *title);
+        // WEBVIEW_API void webview_set_fullscreen(struct webview *w, int fullscreen);
+        // WEBVIEW_API void webview_set_color(struct webview *w, uint8_t r, uint8_t g,
+        //                                    uint8_t b, uint8_t a);
+        // WEBVIEW_API void webview_dialog(struct webview *w,
+        //                                 enum webview_dialog_type dlgtype, int flags,
+        //                                 const char *title, const char *arg,
+        //                                 char *result, size_t resultsz);
+        // WEBVIEW_API void webview_dispatch(struct webview *w, webview_dispatch_fn fn,
+        //                                   void *arg);
+        // WEBVIEW_API void webview_terminate(struct webview *w);
+        // WEBVIEW_API void webview_exit(struct webview *w);
+        [DllImport("webview", CallingConvention = CallingConvention.Cdecl)]
+        public static extern void webview_exit(UIntPtr webview);
+        // WEBVIEW_API void webview_debug(const char *format, ...);
+        // WEBVIEW_API void webview_print_log(const char *s);
     }
 }
--- Program.cs
+++ Program.cs
@@ -3,15 +3,32 @@ using System.Runtime.InteropServices;
 
 namespace webview_cs
 {
+    using static Ffi;
+
     class Program
     {
         static void Main(string[] args)
         {
-            Ffi.webview("Foo bar",
-            "https://willspeak.me",
-            800,
-            600,
-            1);
+            var webview = webview_alloc(
+                "Foo bar",
+                "https://google.com",
+                1024,
+                768,
+                1,
+                0,
+                new webview_external_invoke_cb_t(WebviewCallback));
+            if (webview == UIntPtr.Zero)
+                throw new Exception("Could not allocate webview");
+            Console.WriteLine("Got webview: {0}", webview);
+            while (webview_loop(webview, 1) == 0)
+                ;
+            webview_exit(webview);
+            webview_release(webview);
+        }
+
+        private static void WebviewCallback(IntPtr webview, string arg)
+        {
+            Console.WriteLine("Webview Callback: {0}", arg);
         }
     }
 }
