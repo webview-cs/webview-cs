63755bb44e0e0421384b44e3dab040a4038663ed
zenakuten <zenakuten@yahoo.com>
2018-06-12 06:01:36 -0700

    fix issue with helloworld not working in windows

--- src/Webview/Content.cs
+++ src/Webview/Content.cs
@@ -9,8 +9,8 @@ namespace Webview
 
         public static IContent FromHtml(string html)
         {
-            var dataUri = new StringBuilder("data:text/html;charset=UTF-8;base64,");
-            dataUri.Append(Convert.ToBase64String(Encoding.UTF8.GetBytes(html)));
+            var dataUri = new StringBuilder("data:text/html,");
+            dataUri.Append(html);
             return new Content(dataUri.ToString());
         }
 
