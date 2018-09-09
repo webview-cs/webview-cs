d701a432e886f9c781dd6d6dfbc1f6b0c9545581
Will Speak <lithiumflame@gmail.com>
2018-09-09 16:55:00 +0100

    Add ASP .NET Core Hosting Example
    This example shows how to use Webview with an embedded ASP .NET Core
    WebHost for a siple application.

---
+++ examples/AspNetCoreHosting/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using Webview.WebHost;
+
+namespace AspNetCoreHosting
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateWebHostBuilder(args)
+                .WithDynamicPort()
+                .WithNoOutput()
+                .Build()
+                .RunWebview();
+        }
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>();
+    }
+}
