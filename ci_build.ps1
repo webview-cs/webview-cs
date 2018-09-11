718623b59c76d7652590de2fce8fb05c68f13894
Will Speak <lithiumflame@gmail.com>
2018-09-11 07:33:48 +0100

    Switch to Build Script for Build
    Runs tests and packages with a version suffix for non-tagged builds.

---
+++ ci_build.ps1
@@ -0,0 +1,25 @@
+## Build
+dotnet build
+
+## Test
+ForEach ($proj in (Get-ChildItem -Path test -Recurse -Filter '*.csproj'))
+{
+	dotnet test --no-restore --no-build $proj.FullName
+}
+
+## Package
+if (${env:APPVEYOR_REPO_TAG} -eq $true)
+{
+	# Tag pushes are packaged with the version in the `.csproj`
+	dotnet pack --no-restore `
+	  -o artifacts_nuget `
+	  --configuration Release
+}
+else
+{
+	# Non-tag pushes are packaged with a pre-release version
+	dotnet pack --no-restore `
+	  -o artifacts_nuget `
+	  --version-suffix=${env:APPVEYOR_REPO_BRANCH}${env:APPVEYOR_BUILD_NUMBER} `
+	  --configuration Release
+}
