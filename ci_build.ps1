440b68da9d2e41fc2a5e3bd3d131959018841785
Will Speak <lithiumflame@gmail.com>
2018-09-12 07:45:13 +0100

    Run Tests for Cofniguration Built
    Update CI build to test the Release build rather than Debug. Fixes up
    test failures due to not finding the test assembly.

--- ci_build.ps1
+++ ci_build.ps1
@@ -4,12 +4,17 @@ dotnet build
 ## Test
 ForEach ($proj in (Get-ChildItem -Path tests -Recurse -Filter '*.csproj'))
 {
-	dotnet test --no-restore --no-build $proj.FullName
+	dotnet test --no-restore `
+		--no-build `
+		--configuration Release `
+		$proj.FullName
 }
 
 ## Package
 if (${env:APPVEYOR_REPO_TAG} -eq $true)
 {
+	Write-Host "Building without suffix"
+	
 	# Tag pushes are packaged with the version in the `.csproj`
 	dotnet pack --no-restore `
 	  -o artifacts_nuget `
@@ -17,9 +22,12 @@ if (${env:APPVEYOR_REPO_TAG} -eq $true)
 }
 else
 {
+	$suffix = ${env:APPVEYOR_REPO_BRANCH}${env:APPVEYOR_BUILD_NUMBER}
+	Write-Host "Building with suffix=$suffix"
+
 	# Non-tag pushes are packaged with a pre-release version
 	dotnet pack --no-restore `
 	  -o artifacts_nuget `
-	  --version-suffix=${env:APPVEYOR_REPO_BRANCH}${env:APPVEYOR_BUILD_NUMBER} `
+	  --version-suffix=$suffix `
 	  --configuration Release
 }
